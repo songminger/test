fc9aa1e899edd6e734b92bab4690632c0b1dc644
SongMing <songminger@gmail.com>
2020-05-22 13:43:29 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,3 +1,4 @@
+ # v2ray
  bash <(curl -L -s https://install.direct/go.sh)
  sudo su
  service v2ray status
@@ -7,3 +8,9 @@
 
 # centos stop firewar (vultr)
 systemctl stop firewalld.service
+
+
+# shadowsocks-go
+wget --no-check-certificate https://raw.githubusercontent.com/iMeiji/shadowsocks_install/master/shadowsocks-go.sh
+chmod +x shadowsocks-go.sh
+./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
